dc5268c77a0e90d55750b6cfcfaa634f19223e26
Dat Vo <vothanhdat@outlook.com>
2025-10-23 00:41:12 +0700

    Add GitHub Pages deployment configuration

--- vite.config.dev.ts
+++ vite.config.dev.ts
@@ -2,10 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/react-state-custom/',
   plugins: [
     react(),
   ],
   build: {
     target: 'esnext',
-  }
+    outDir: 'demo-dist',
+  },
+  server: {
+    port: 3000,
+  },
 });
