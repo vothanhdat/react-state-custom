e91b85714ad39975d6956ec87743f4021bf2f62b
Dat Vo <vothanhdat@outlook.com>
2025-10-23 00:32:30 +0700

    Update

--- src/Test.tsx
+++
@@ -1,5 +0,0 @@
-import { Playground } from './Playground'
-
-export const Test = () => {
-    return <Playground />
-}
--- src/dev.tsx
+++ src/dev.tsx
@@ -1,23 +1,7 @@
 import { createRoot } from 'react-dom/client'
-import { DevToolContainer } from './dev-tool/DevTool'
-import { AutoRootCtx, createAutoCtx } from './state-utils/createAutoCtx'
-import { Test } from './Test'
-import { DataViewComponent } from './dev-tool/DataViewComponent'
-import { ObjectView } from "react-obj-view"
+import { Playground } from './examples/Playground'
 import "react-obj-view/dist/react-obj-view.css"
 
 
-const DataView: DataViewComponent = ({ name, value }) => {
-  return <ObjectView
-    {...{ name, value }}
-    expandLevel={1}
-  />
-}
-
-createRoot(document.getElementById('root')!).render(
-  <>
-    {/* <DevToolContainer Component={DataView} /> */}
-    <Test />
-    <AutoRootCtx />
-  </>
-)
+createRoot(document.getElementById('root')!)
+  .render(<Playground />)
