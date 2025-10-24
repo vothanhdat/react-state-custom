b1dea076382cfa6a2351f7b5922b0c5a96d4e57e
Dat Vo <vothanhdat@outlook.com>
2025-10-24 15:09:40 +0700

    fix: correct key prop assignment in StateLabelRender mapping

--- src/dev-tool/DevToolState.tsx
+++ src/dev-tool/DevToolState.tsx
@@ -50,7 +50,7 @@ export const DevToolState: React.FC<{ Component: DataViewComponent }> = ({ Compo
                     .map(e => JSON.parse(e)?.[0])
                     .filter(e => e != "auto-ctx" && e)
                     .filter(filterFn)
-                    .map(currentKey => <StateLabelRender {...{key:currentKey, selectedKey, setKey, currentKey }} />)}
+                    .map(currentKey => <StateLabelRender key={currentKey} {...{ selectedKey, setKey, currentKey }} />)}
             </HightlightWrapper>
 
         </div>
